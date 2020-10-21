752d75f888173baa75b08897c0a52fc0de64f7b7
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2020-10-21 02:03:21 -0400

    add listing and user models

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from datetime import datetime
+from users.models import User
 
-# Create your models here.
+
+class Listing(models.Model):
+    # each listing is connected w/ a authenticated user
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    # todo: take from car table car_year, car_make, car_model, car_category
+    car = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    borough = models.CharField(max_length=100)
+    price = models.IntegerField()
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
+    is_rented = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.first_name
