6ad2cec835411363951b974d119578a626c9b459
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2020-11-11 01:39:35 -0500

    update search form so it works

--- listings/views.py
+++ listings/views.py
@@ -31,9 +31,38 @@ def listing(request, listing_id):
 
 
 def search(request):
-    listings = Listing.objects.order_by('-list_date').filter(is_published=True)
+    queryset_list = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)
 
-    paginator = Paginator(listings, 6)
+    # Car
+    if 'cars' in request.GET:
+        cars = request.GET['cars']
+        if cars:
+            queryset_list = queryset_list.filter(
+                car__icontains=cars)
+
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(
+                description__icontains=keywords)
+
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(
+                price__lte=price)
+
+    # Boroughs
+    if 'borough' in request.GET:
+        borough = request.GET['borough']
+        if borough:
+            queryset_list = queryset_list.filter(
+                borough__iexact=borough)
+
+    paginator = Paginator(queryset_list, 6)
     page = request.GET.get('page')
     paged_listings = paginator.get_page(page)
 
