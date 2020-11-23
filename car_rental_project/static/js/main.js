6f29e6bf0d94ce723114713444e530c989b775d4
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2020-11-22 19:51:32 -0500

    implement alerts using django messages framework

--- car_rental_project/static/js/main.js
+++ car_rental_project/static/js/main.js
@@ -1,2 +1,16 @@
 const date = new Date();
 document.querySelector(".year").innerHTML = date.getFullYear();
+
+document.addEventListener("DOMContentLoaded", () => {
+  (document.querySelectorAll(".notification .delete") || []).forEach(($delete) => {
+    var $notification = $delete.parentNode;
+
+    $delete.addEventListener("click", () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
+
+setTimeout(function () {
+  $("#message").fadeOut("slow");
+}, 5000);
--- users/views.py
+++ users/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def register(request):
-    return render(request, 'users/register.html')
+    if request.method == 'POST':
+        messages.error(request, 'Testing error message')
+        return redirect('register')
+    else:
+        return render(request, 'users/register.html')
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        # login user
+        return
+    else:
+        return render(request, 'users/login.html')
 
 
 def logout(request):
