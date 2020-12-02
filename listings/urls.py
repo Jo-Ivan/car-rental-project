1a92ef0ef9ba340ad694887d89d8e25380b0c028
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2020-12-01 22:21:38 -0500

    minor auth checks

--- listings/urls.py
+++ listings/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='listings'),
-    path('view/<int:listing_id>', views.listing, name='listing'),
+    path('<int:listing_id>', views.listing, name='listing'),
     path('search', views.search, name='search'),
     path('listing_create', views.listing_create, name='listing_create'),
     path('delete/<int:listing_id>',
