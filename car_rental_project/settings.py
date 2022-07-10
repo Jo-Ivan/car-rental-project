51f12c0e00aaa52fb2bef0e45d8c3d8b3112f37c
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2022-07-10 17:37:10 -0400

    temp settings change

--- car_rental_project/settings.py
+++ car_rental_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = '$&6y1=phwx&j_w6)0b2yg3ox1rg=#6=-sztbb9+7r0)rjufwpo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -144,11 +144,11 @@ MESSAGE_TAGS = {
 }
 
 # Email config
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER='car.rental.project.hunter@gmail.com'
+EMAIL_HOST_PASSWORD='carrentalproject'
+EMAIL_USE_TLS=True
 
 try:
     from .local_settings import *
